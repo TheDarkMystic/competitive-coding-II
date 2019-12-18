56e2e8bf76b6548e6f14db96462bc68ce1fcd300
TheDarkMystic <er.jayk@gmail.com>
2019-12-18 22:23:35 +0530

    Add Solutions to src/DynamicProgramming

--- src/DynamicProgramming/Classic/CoinChange.java
+++ src/DynamicProgramming/Classic/CoinChange.java
@@ -41,5 +41,63 @@ package DynamicProgramming.Classic;
  the number of coins is less than 500
  the answer is guaranteed to fit into signed 32-bit integer
  */
-public class CoinChange {
+
+class CoinChange {
+    public static void main(String args[]){
+        CoinChangeSolver solver= new CoinChangeSolver();
+        int[] coins={1, 2, 5};
+        int amount=5;
+        System.out.println(solver.change(amount,coins));
+        //System.out.println(solver.solveRecursive(steps));
+    }
+}
+
+class CoinChangeSolver{
+    public int change(int totalAmount, int[] coins) {
+        // the solution is very similar to the knapsack problem
+        int numOfCoins=coins.length;
+        // we will take amount on the X axis and available coins on the Y axis
+        int dp[][]=new int[numOfCoins+1][totalAmount+1];
+
+        for(int avlblCoins=0;avlblCoins<=numOfCoins; avlblCoins++){
+            for(int amountToMake=0; amountToMake<=totalAmount; amountToMake++){
+
+                // when we don't have any coins and we want to make amount 0, this can be done in
+                // one way, not including anything
+                if(avlblCoins==0 && amountToMake==0){
+                    dp[avlblCoins][amountToMake]=1;
+                    continue;
+                }
+                // we have no coins and we want to make amounts in the range 1..amount
+                // this cannot be achieved
+                if(avlblCoins==0 && amountToMake!=0){
+                    dp[avlblCoins][amountToMake]=0;
+                    continue;
+                }
+                // when we have coins ranging from coins[0] to coins[len-1] and we have to
+                // make amount=0, this is possible in one way, not including any coin
+                if(avlblCoins!=0 && amountToMake==0){
+                    dp[avlblCoins][amountToMake]=1;
+                    continue;
+                }
+                int currentCoinValue = coins[avlblCoins-1];
+                // number of ways in which amount can be formed without using current coin
+                int withoutThisCoin = dp[avlblCoins-1][amountToMake];
+
+                // number of ways in which amount can be formed using current coin
+                int withThisCoin=0;
+                if(amountToMake>=currentCoinValue){
+                    withThisCoin =dp[avlblCoins][amountToMake-currentCoinValue];
+                }
+                else
+                    withThisCoin=0;
+
+                //total number of ways in which amount can be formed
+                dp[avlblCoins][amountToMake]=withoutThisCoin+withThisCoin;
+            }
+        }
+
+
+        return dp[numOfCoins][totalAmount];
+    }
 }
--- src/DynamicProgramming/Classic/LongestIncreasingSubsequence.java
+++ src/DynamicProgramming/Classic/LongestIncreasingSubsequence.java
@@ -3,5 +3,50 @@ package DynamicProgramming.Classic;
 /**
  * @author jakadam on 2019-12-17
  */
+
+import java.util.Arrays;
+
+/**
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ *
+ * Example:
+ *
+ * Input: [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ * Note:
+ *
+ * There may be more than one LIS combination, it is only necessary for you to return the length.
+ * Your algorithm should run in O(n2) complexity.
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ */
 public class LongestIncreasingSubsequence {
+   public static void main(String args[]){
+       LongestIncreasingSubsequenceSolver solver= new LongestIncreasingSubsequenceSolver();
+       int A[]={10,9,2,5,3,7,101,18};
+       System.out.println(solver.solve(A));
+   }
+
+}
+class LongestIncreasingSubsequenceSolver {
+    public int solve(int[] A){
+        int len=A.length;
+        int[] maxLIS=new int[len+1];
+
+        Arrays.fill(maxLIS,1);
+
+        int maxSoFar=1;
+
+
+        for(int i=1; i<len; i++){
+            for(int j=0; j<i; j++){
+
+                if(A[i]>A[j]){
+                    maxLIS[i]=Math.max(maxLIS[i],maxLIS[j]+1);
+                }
+            }
+            maxSoFar=Math.max(maxSoFar,maxLIS[i]);
+        }
+        return maxSoFar;
+    }
 }
