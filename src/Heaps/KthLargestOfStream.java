454b9873768524bd5756a7decb960374b253546c
TheDarkMystic <er.jayk@gmail.com>
2019-12-21 16:23:34 +0530

    Add Solutions to src/Heaps

---
+++ src/Heaps/KthLargestOfStream.java
@@ -0,0 +1,64 @@
+package Heaps;
+
+/**
+ * @author jakadam on 2019-12-21
+ */
+
+import java.util.PriorityQueue;
+
+/**
+ * Design a class to find the kth largest element in a stream. Note that it is the
+ * kth largest element in the sorted order, not the kth distinct element.
+ *
+ * Your KthLargest class will have a constructor which accepts an integer k and
+ * an integer array nums, which contains initial elements from the stream. For each
+ * call to the method KthLargest.add, return the element representing the kth largest
+ * element in the stream.
+ *
+ * Example:
+ *
+ * int k = 3;
+ * int[] arr = [4,5,8,2];
+ * KthLargest kthLargest = new KthLargest(3, arr);
+ * kthLargest.add(3);   // returns 4
+ * kthLargest.add(5);   // returns 5
+ * kthLargest.add(10);  // returns 5
+ * kthLargest.add(9);   // returns 8
+ * kthLargest.add(4);   // returns 8
+ *
+ * Note:
+ * You may assume that nums' length ≥ k-1 and k ≥ 1.
+ */
+
+class KthLargestOfStream {
+    /**
+     * Keep track of the k biggest elements in the minimum priority queue q. q.peek() is the answer.
+     */
+    PriorityQueue<Integer> heap;
+    int k;
+    public KthLargestOfStream(int k,int[] nums){
+        this.k=k;
+        heap= new PriorityQueue<Integer>(k);
+        for(int num:nums){
+            add(num);
+        }
+    }
+
+    public int add(int num){
+        //add the number to the heap
+        heap.add(num);
+
+        //if heap size has increased than the set capacity, remove the root
+        if(heap.size()>k)
+            heap.poll();
+
+        //return the kth largest number
+        return heap.peek();
+
+    }
+}
+/**
+ *  Tested on leetcode
+ *  https://leetcode.com/problems/kth-largest-element-in-a-stream/submissions/
+ *
+ */
