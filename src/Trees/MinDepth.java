ec9ac1d91f22a630176b7a46528c66b573d201c3
TheDarkMystic <er.jayk@gmail.com>
2020-01-23 22:16:50 +0530

    Add Solutions to src/MinDepth

--- src/Trees/MinDepth.java
+++ src/Trees/MinDepth.java
@@ -37,19 +37,29 @@ public class MinDepth {
         System.out.println(minDepth(tree.getRoot()));
     }
 
-
     public static int minDepth(TreeNode root) {
-        //check if root is null
-        if(root==null)
-            return 0;
-        // check if root is leaf node, return 1, as depth of leaf node is 1
-        if(root.left==null && root.right==null)
-            return 1;
 
-        int leftMinDepth= root.left!=null ? minDepth(root.left) : Integer.MAX_VALUE;
-        int rightMinDepth= root.right!=null ? minDepth(root.right) : Integer.MAX_VALUE;
+            // min depth is is the distance of the root node to nearest leaf node
+            // so the task boils down to finding the nearest leaf node
+            // 1. using DFS
+            //check if root is null
+            if(root==null)
+                return 0;
+            // check if root is leaf node, return 1, as depth of leaf node is 1
+            if(root.left==null && root.right==null)
+                return 1;
 
-        return 1+Math.min(leftMinDepth,rightMinDepth);
+            //if left subtree is null, explore right subtree for nearest leaf node
+            if(root.left==null)
+                return 1+minDepth(root.right);
+            //if right subtree is null, explore left subtree for nearest leaf node
+            if(root.right==null)
+                return 1+minDepth(root.left);
+            // if both subtrees are not null, return minimum of both the subtree depths
+            return 1+Math.min(minDepth(root.left),minDepth(root.right));
+
+            // 2. using BSF, use level order traversal and explore nodes levelwise,
+            // stop as soon as we find leaf node, return the level number
     }
 }
 
